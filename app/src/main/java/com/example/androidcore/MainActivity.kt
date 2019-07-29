896efaebcb555b85447d4dde240a079dc5ee4179
Andrea Gazzi <andrea.gazzi@reti.it>
2019-07-29 14:15:34 +0200

    modified layout file and changed text color by code

---
+++ app/src/main/java/com/example/androidcore/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.androidcore
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.setTextColor(resources.getColor(R.color.colorAccent));
+
+
+    }
+}
