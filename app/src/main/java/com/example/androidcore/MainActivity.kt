ec708e441eac92de14e2b534c6a99edcd0ad38c3
Andrea Gazzi <andrea.gazzi@reti.it>
2019-07-30 13:00:04 +0200

    added ayout file and modified color background in listeners

--- app/src/main/java/com/example/androidcore/MainActivity.kt
+++ app/src/main/java/com/example/androidcore/MainActivity.kt
@@ -3,15 +3,26 @@ package com.example.androidcore
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.relative_layout.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.relative_layout);
 
-        textView.setTextColor(resources.getColor(R.color.colorAccent));
+        button1.setOnClickListener { view ->
+            container.setBackgroundColor(resources.getColor(R.color.colorAccent))
+        }
 
+
+        button2.setOnClickListener { view ->
+            container.setBackgroundColor(resources.getColor(R.color.colorPrimaryDark))
+        }
+
+        button3.setOnClickListener { view ->
+            container.setBackgroundColor(resources.getColor(R.color.colorPrimary))
+        }
 
     }
 }
