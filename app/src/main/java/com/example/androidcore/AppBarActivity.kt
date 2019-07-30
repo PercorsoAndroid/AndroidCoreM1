bc6e20299c1c7a8bded48ba6cd47ae567079a0b9
Andrea Gazzi <andrea.gazzi@reti.it>
2019-07-30 16:32:39 +0200

    aggiunto menu overflow all'appbar

--- app/src/main/java/com/example/androidcore/AppBarActivity.kt
+++ app/src/main/java/com/example/androidcore/AppBarActivity.kt
@@ -2,6 +2,8 @@ package com.example.androidcore
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_app_bar.*
 
 class AppBarActivity : AppCompatActivity() {
@@ -10,5 +12,24 @@ class AppBarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_bar)
         setSupportActionBar(my_toolbar);
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar_menu, menu);
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem)= when (item.itemId){
+        R.id.action_favorite -> {
+            true;
+        }
+
+        R.id.action_settings -> {
+            true;
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
