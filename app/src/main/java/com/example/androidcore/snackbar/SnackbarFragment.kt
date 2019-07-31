b8795165611ecd15a6a874b58881c1e452dcb021
Andrea Gazzi <andrea.gazzi@reti.it>
2019-07-31 16:29:04 +0200

    Added new fragment for snackbar and snackbar show

---
+++ app/src/main/java/com/example/androidcore/snackbar/SnackbarFragment.kt
@@ -0,0 +1,28 @@
+package com.example.androidcore.snackbar
+
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.androidcore.R
+import kotlinx.android.synthetic.main.fragment_snackbar.*
+
+class SnackbarFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_snackbar, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Snackbar.make(coordinatorLayout, resources.getString(R.string.helloSnackbarFragment), Snackbar.LENGTH_SHORT).show();
+
+    }
+
+
+}
--- app/src/main/java/com/example/androidcore/toast/ToastFragment.kt
+++ app/src/main/java/com/example/androidcore/toast/ToastFragment.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 
 import com.example.androidcore.R
+import kotlinx.android.synthetic.main.fragment_toast.*
 
 class ToastFragment : Fragment() {
 
@@ -20,6 +22,10 @@ class ToastFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         Toast.makeText(this.context, resources.getString(R.string.toast_fragment_title), Toast.LENGTH_SHORT).show();
+
+        toSnackbarFragmentBtn.setOnClickListener { view ->
+            findNavController().navigate(R.id.action_toastFragment_to_snackbarFragment);
+        }
 
     }
 
